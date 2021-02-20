e2381393bf4d8dea2b8413695c4b19e916165451
Caio Tavares <caio.tavares11@gmail.com>
2021-02-20 20:11:38 -0300

    Updating formulae

--- pkg/brew/emv.rb
+++ pkg/brew/emv.rb
@@ -1,8 +1,7 @@
 class Emv < Formula
-  version '0.1.0'
   desc "EMV tooling"
   homepage "https://github.com/caiotavares/emv"
-  url "https://github.com/caiotavares/emv/releases/download/v#{version}/emv-#{version}-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/caiotavares/emv/releases/download/v0.1.0/emv-0.1.0-x86_64-apple-darwin.tar.gz"
   sha256 "ffbd5c5f27f707e7aa62b6b67c3e8f69d78c597577c63a3b93d524e94d0c333f"
   license "MIT"
 
